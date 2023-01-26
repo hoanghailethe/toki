ca0db90058f499433d8dcf544c39747fa1750985
hoanghailethe <lethehoanghai@gmail.com>
2023-01-26 15:53:34 +0700

    NavBar Component

---
+++ Components/Navbar.tsx
@@ -0,0 +1,10 @@
+//rafce
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,27 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect, useState } from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  const [isSSR, setIsSSR] = useState(true) ;
+
+  useEffect(() => {
+    setIsSSR(false)
+  }, []) ;
+
+  if(isSSR) return null ;
+
+  return (
+    <div>
+        NavBar 
+        
+        <div className="flex gap-6 md:gap-20">
+
+        </div>
+        <Component {...pageProps} />
+
+    </div>
+
+  );
 }
